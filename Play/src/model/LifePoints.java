aaf2227c1a827e5e79b66326181cb5689817df01
hamzashmi <amrnihamza@gmail.com>
2023-12-04 14:04:49 +0100

    waiting for le compteur

--- Play/src/model/LifePoints.java
+++ Play/src/model/LifePoints.java
@@ -5,13 +5,14 @@ import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LifePoints extends SuperObject {
+public class LifePoints{
     public Interface Inter;
     private int LP;
     private int max_LP;
     private List<ImageIcon> Current_lifepoints;
     private int x;  // You need to define the x and y variables
     private int y;
+    public int c;
 
     public ImageIcon[] Hearts = {
         new ImageIcon(getClass().getResource("/images/wall/ui_heart_full.png")),
@@ -21,18 +22,37 @@ public class LifePoints extends SuperObject {
     public LifePoints(Interface Inter) {
         this.Inter = Inter;
         
-        this.x = 0;  // Set initial values for x and y
-        this.y = 0;
+        this.c = 2;  // Set initial values for x and y
+       
     }
 
     
     public void draw(Graphics2D g2) {
       
-
+        if(c==0){
           // Ensure the index is within bounds
         g2.drawImage(Hearts[0].getImage(), 20, 40, Inter.titleSize, Inter.titleSize, null);
         g2.drawImage(Hearts[0].getImage(), 55, 40, Inter.titleSize, Inter.titleSize, null);
         g2.drawImage(Hearts[0].getImage(), 90, 40, Inter.titleSize, Inter.titleSize, null);
+        }
+        if(c==1){
+          // Ensure the index is within bounds
+        g2.drawImage(Hearts[0].getImage(), 20, 40, Inter.titleSize, Inter.titleSize, null);
+        g2.drawImage(Hearts[0].getImage(), 55, 40, Inter.titleSize, Inter.titleSize, null);
+        g2.drawImage(Hearts[1].getImage(), 90, 40, Inter.titleSize, Inter.titleSize, null);
+        }
+        if(c==2){
+          // Ensure the index is within bounds
+        g2.drawImage(Hearts[0].getImage(), 20, 40, Inter.titleSize, Inter.titleSize, null);
+        g2.drawImage(Hearts[1].getImage(), 55, 40, Inter.titleSize, Inter.titleSize, null);
+        g2.drawImage(Hearts[1].getImage(), 90, 40, Inter.titleSize, Inter.titleSize, null);
+        }
+        if(c>=3){
+          // Ensure the index is within bounds
+        g2.drawImage(Hearts[1].getImage(), 20, 40, Inter.titleSize, Inter.titleSize, null);
+        g2.drawImage(Hearts[1].getImage(), 55, 40, Inter.titleSize, Inter.titleSize, null);
+        g2.drawImage(Hearts[1].getImage(), 90, 40, Inter.titleSize, Inter.titleSize, null);
+        }
 
         
 
